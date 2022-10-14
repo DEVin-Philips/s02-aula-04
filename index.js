7f1352bede947e125511eaba19ce06cd49aee892
Thais Bertoldo <thaisbertoldo11@gmail.com>
2022-10-14 20:51:23 -0300

    aula 04

---
+++ index.js
@@ -0,0 +1,17 @@
+// for (var count = 0; count <= 10; count++) {
+//   console.log('count', count);
+// }
+
+// for (var count = 10; count >= 0; count--) {
+//   console.log('count', count);
+// }
+
+// var sair = false;
+
+// while (!sair) {
+//   sair = confirm('Deseja sair?');
+// }
+var sair = true;
+do {
+  sair = confirm('Deseja sair?');
+} while (!sair);
